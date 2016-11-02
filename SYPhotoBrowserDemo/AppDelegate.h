34396f07a0a778f063c774a23a26e7000f06ad38
孙扬 <624162319@qq.com>
2016-11-02 09:32:45 +0800

    Initial Commit

---
+++ SYPhotoBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYPhotoBrowserDemo
+//
+//  Created by 孙扬 on 2016/11/2.
+//  Copyright © 2016年 孙扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYPhotoBrowserDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SYPhotoBrowserDemo
+//
+//  Created by 孙扬 on 2016/11/2.
+//  Copyright © 2016年 孙扬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
